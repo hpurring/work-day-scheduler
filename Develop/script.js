32cfb358f3668103e42a52cc4d71df57101825db
hpurring <hilarypurrington@gmail.com>
2021-11-07 08:14:42 -0500

    colorize hour blocks

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var currentDay = document.querySelector("#currentDay");
 var showCurrentDay = function() {
     document.querySelector("#currentDay").innerHTML = 
     `
-    <p>${moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
+    <p>${moment().format('MMMM Do YYYY, h:mm a')}</p>
     `;
 };
 
@@ -14,15 +14,15 @@ showCurrentDay();
 function updateTimeSlotItems() {
     //assign classes (past | present | future)
 
-    $("time-block").each(function() {
+    $(".time-block").each(function() {
         if (moment().isBefore(currentHour)) {
-            $("time-block").addClass("past");
+            $(".time-block").addClass("past");
           } else if (moment().isAfter(currentHour)) {
-              $("time-block").addClass("future");
-              $("time-block").removeClass("past", "present");
+              $(".time-block").addClass("future");
+              $(".time-block").removeClass("past", "present");
           } else {
-              $("time-block").addClass("present");
-              $("time-block").removeClass("past", "future");
+              $(".time-block").addClass("present");
+              $(".time-block").removeClass("past", "future");
           };
     });
 };
@@ -38,7 +38,7 @@ function handleSave(e) {
 };
 
 function main() {
-    $("#currentDay").text(moment().format('dddd, LL'));
+    showCurrentDay();
     updateTimeSlotItems();
     // add click event to timeSlots
     $(document).on('click', '.save-btn', handleSave);
